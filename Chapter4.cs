d147ec361a6d1622db3c23bf5c030d97a2cb89d6
Ethan <enephew2@mail.valenciacollege.edu>
2021-06-22 21:47:09 -0400

    Working on Chapter 4.

---
+++ Chapter4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * Chapter 4 is about methods and their behavior. 
+ */
+
+namespace C_sharp_Programming
+{
+    class Chapter4
+    {
+        public static void SquareExample()
+        {
+            int aValue = 786;
+            int result;
+
+            result = aValue * aValue;
+            Console.WriteLine("{0} sqared is {1}", aValue, result);
+            Console.Read();
+        }
+    }
+}
--- Main_Method.cs
+++ Main_Method.cs
@@ -14,6 +14,8 @@ namespace C_sharp_Programming
 
             Chapter3_Carpet_Calculator bestCarpet = new Chapter3_Carpet_Calculator("best" , 27.95); 
             Chapter3_Carpet_Calculator econCarpet = new Chapter3_Carpet_Calculator("econ", 15.95);
+
+            Chapter4.SquareExample();
         }
     }
 
