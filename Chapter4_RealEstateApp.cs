70d294eb1e33a340cec8235e88d20df259d0aa38
Ethan <enephew2@mail.valenciacollege.edu>
2021-06-24 19:50:11 -0400

    Chapter 4. Working on chapter project.

---
+++ Chapter4_RealEstateApp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_sharp_Programming
+{
+    class Chapter4_RealEstateApp
+    {
+        /*
+         * This will have all the run commands for the application. 
+         * I want to do all my notes and projects with a single
+         * main method, because it is easier for me to look over 
+         * my notes and projects that way. 
+         */
+        public static void RunApp()
+        {
+
+        }
+    }
+
+    class Chapter4_RealEstateInvestment
+    {
+        int yearBuilt;
+        double purchasePrice;
+        string streetAddress;
+        double monthlyExpense;
+        double incomeFromRent;
+
+        public int YearBuilt
+        {
+            get // getter for the yearBuilt
+            {
+                return yearBuilt;
+            }
+        }
+
+        public double PurchasePrice
+        {
+            get
+            {
+                return purchasePrice; 
+            }
+        }
+
+        public Chapter4_RealEstateInvestment(int yearBuilt, double purchasePrice, string streetAddress)
+        {
+            this.yearBuilt = yearBuilt;
+            this.purchasePrice = purchasePrice;
+            this.streetAddress = streetAddress;
+        }
+    }
+}
