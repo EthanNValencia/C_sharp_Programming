8775bc007541e7f29fbd0ddb1b3b4a72bdc4de8d
Ethan <enephew2@mail.valenciacollege.edu>
2021-07-19 19:31:29 -0400

    Still working on chapter 7, arrays.

--- Chapter7.cs
+++ Chapter7.cs
@@ -44,6 +44,87 @@ namespace C_sharp_Programming
                 total += val;
             }
             Console.WriteLine("Sum of the score array is: {0}", total);
+
+            /*
+             * System.Array class methods, pg 343-344
+             */
+            int myIndex = Array.BinarySearch(anArray, 200);
+            Console.WriteLine("The Array.BinarySearch returns: " + myIndex);
+            Array.Clear(anArray, 1, 1);
+            Console.WriteLine("The Array.Clear: " + anArray[1]); // Assigns what is in the specified position to 1. 
+            int[] cloneAnArray = (int[]) anArray.Clone(); // Clones the array, but it needs a cast. 
+        }
+        public void BinarySearch() // Array.BinarySearch example, pg 343
+        {
+            double[] waterDepth = { 2.4, 3.5, 6.8 };
+            double x = 6.8;
+            int myIndex = Array.BinarySearch(waterDepth, x);
+            Console.WriteLine(myIndex);
+        }
+        public void Clear() // Array.Clear example, pg 343
+        {
+            double[] waterDepth = { 2.4, 3.5, 6.8 };
+            double x = 6.8;
+            Array.Clear(waterDepth, 1, 1);
+            Console.WriteLine(waterDepth[1]); // prints 0
+        }
+        public void Clone() // Array.Clone example, pg 343
+        {
+            double[] waterDepth = { 2.4, 3.5, 6.8 };
+            double[] w = new double[3];
+            object o = waterDepth.Clone();
+            w = (double[]) o;
+        }
+        public void Copy() // Array.Copy example, pg 343 
+        {
+            double[] waterDepth = { 2.4, 3.5, 6.8 };
+            double[] w = new double[3];
+            Array.Copy(waterDepth, 0, w, 0, 3); // This copies waterDepth to w. 
+        }
+        public void CopyTo() // Array.CopyTo example, pg 343
+        {
+            double[] waterDepth = { 2.4, 3.5, 6.8 };
+            double[] w = new double[5];
+            waterDepth.CopyTo(w, 2); // This copies the array, but it specifies the begin point to index 2. 
+            Console.WriteLine(w[3]); // prints 3.5
+        }
+        public void GetValue() // Array.GetValue example, pg 343 
+        {
+            double[] waterDepth = { 2.4, 3.5, 6.8 };
+            Console.WriteLine(waterDepth.GetValue(1)); // prints 3.5
+        }
+        public void IndexOf() // Array.IndexOf example, pg 344 
+        {
+            double[] waterDepth = { 2.4, 3.5, 6.8 };
+            int i = Array.IndexOf(waterDepth, 3.5);
+            Console.WriteLine(i); // This would print 1 (returns -1 if the value is not found in the array)
+        }
+        public void LastIndexOf() // Array.LastIndexOf example, pg 344 
+        {
+            double[] waterDepth = { 2.4, 3.5, 6.8, 2.4 }; // 2.4 occurs at 0 and at 3
+            int i = Array.LastIndexOf(waterDepth, 2.4);
+            Console.WriteLine(i); // This would print 3 
+        }
+        public void Reverse() // Array.Reverse example, pg 344 
+        {
+            double[] waterDepth = { 2.4, 3.5, 6.8 }; 
+            Array.Reverse(waterDepth);
+            foreach (double wVal in waterDepth)
+                Console.Write(wVal + " "); // Displays 6.8 3.5 2.4
+        }
+        public void SetValue() // Array.SetValue example, pg 344 
+        {
+            double[] waterDepth = { 2.4, 3.5, 6.8 }; 
+            waterDepth.SetValue(55, 0);
+            foreach (double wVal in waterDepth)
+                Console.Write(wVal + " "); // Displays 55 3.5 2.4
+        }
+        public void Sort() // Array.Sort example, pg 344 
+        {
+            double[] waterDepth = { 12.4, 3.5, 6.8 }; 
+            Array.Sort(waterDepth);
+            foreach (double wVal in waterDepth)
+                Console.Write(wVal + " "); // Displays 3.5 6.8 12.4
         }
     }
 }
