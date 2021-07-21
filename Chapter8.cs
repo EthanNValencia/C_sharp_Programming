23492db7e5d69de356420c3275a387850b3193c4
Ethan <enephew2@mail.valenciacollege.edu>
2021-07-21 16:00:50 -0400

    Beginning chapter 8.

---
+++ Chapter8.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*
+ * Introduction to Windows Programming
+ * - Differentiate between the functions of Windows applications and console applications. 
+ * - Learn about graphical user interfaces. 
+ * - Become aware of some elements of good design.
+ * - Use C# and Visual Studio to createa Windows-based applications.
+ * - Create Windows forms and be able to change form properties. 
+ * - Add control objects such as buttons, labels, and text boxes to a form.
+ */
+namespace C_sharp_Programming
+{
+    class Chapter8
+    {
+    }
+}
