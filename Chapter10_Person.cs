aada0a07213a71e8cc5f2a8d13c40908c29e4cb2
Ethan <enephew2@mail.valenciacollege.edu>
2021-07-25 15:58:07 -0400

    Working on OOP Person Student example.

---
+++ Chapter10_Person.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_sharp_Programming
+{
+    class Chapter10_Person // Example 10-1, pg 582
+    {
+        private string idNumber;
+        private string lastName;
+        private string firstName;
+        private int age;
+
+        public string IDNumber 
+        {
+            get
+            {
+                return idNumber;
+            }
+            set
+            {
+                idNumber = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        public Chapter10_Person()
+        {
+            idNumber = "";
+            lastName = "unknown";
+            firstName = string.Empty;
+            age = 0;
+        }
+        public Chapter10_Person(string id)
+        {
+            idNumber = id;
+            lastName = "unknown";
+            firstName = string.Empty;
+            age = 0;
+        }
+        public Chapter10_Person(string id, string lname, string fname)
+        {
+            idNumber = id;
+            lastName = lname;
+            firstName = fname;
+            age = 0;
+        }
+        public Chapter10_Person(string id, string lname, string fname, int anAge)
+        {
+            idNumber = id;
+            lastName = lname;
+            firstName = fname;
+            age = anAge;
+        }
+        public override string ToString()
+        {
+            /*
+             * In the object class, the ToString method looks like this:
+             * public virtual string ToString();
+             */
+            return firstName + " " + lastName;
+        }
+        public virtual int getSleepAmt() // Virtual means that the method can be overridden in the child class. 
+        {
+            return 8;
+        }
+    }
+}
---
+++ Chapter10_Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_sharp_Programming
+{
+    class Chapter10_Student : Chapter10_Person // Student is derrived from Person, a Student IS A Person.
+    {
+        private string major;
+        private int studentId;
+        public Chapter10_Student() : base() // This means that no arguments being sent to base() constructor. 
+        {
+            major = "unknown";
+            studentId = 0;
+        }
+        public Chapter10_Student(string id, string fname, string lname, string maj, int sId) : base(id, lname, fname)
+        {
+            /*
+             * id, lname, and fname are sent to the base 3-param constructor. 
+             */
+            major = maj;
+            studentId = sId;
+        }
+        public override int getSleepAmt()
+        {
+            return 6;
+        }
+    }
+}
