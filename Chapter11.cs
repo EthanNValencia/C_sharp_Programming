2456fc2541c207d52c108f17a59fcb13fd476a9a
Ethan <enephew2@mail.valenciacollege.edu>
2021-07-28 15:23:02 -0400

    Beginning chapter 11.

---
+++ Chapter11.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*
+ * Chapter 11 - Debugging and Handling Exceptions
+ * - Learn about exceptions, how they are thrown and caught.
+ * - Gain an understanding of the different types of errors that are found in programs. 
+ * - Look at debugging methods available in Visual Studio.
+ * - Discover how the Debugger can be used to find runtime errrors.
+ * - Become aware of and use exception-handling techniques: try, catch, finally clauses.
+ * - Explore exception classes and learn how to write and order multple catch clauses.
+ */
+namespace C_sharp_Programming
+{
+    class Chapter11
+    {
+    }
+}
