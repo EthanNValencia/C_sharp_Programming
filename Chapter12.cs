0055e334a1b80327622825d8c6abbc8649bca3e3
Ethan <enephew2@mail.valenciacollege.edu>
2021-07-30 11:28:07 -0400

    Beginning chapter 12.

---
+++ Chapter12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*
+ *              -- Chapter 12 Working with Files --
+ * - Learn about the System.IO namespace
+ * - Explore the FIle and Directory classes
+ * - Constrast the FileInfo and DirectoryInfo classes to the File and Directory classes
+ * - Discover how stream classes are used
+ * - Read data from text files
+ * - Write data to text files
+ * - Explore appending data to text files
+ * - Use exception-handling techniques to process text files
+ * - Read from and write to binary files
+ */
+namespace C_sharp_Programming
+{
+    class Chapter12
+    {
+    }
+}
