717804a244b0672a4431463f2f6c3689aaca920e
Ethan <enephew2@mail.valenciacollege.edu>
2021-08-03 13:54:21 -0400

    Going to work on my first ASP.NET website.

--- Chapter14.cs
+++ Chapter14.cs
@@ -11,6 +11,26 @@ using System.Text;
  * - Add validation, custom, and composite controls to verify user input, display calenders, and connect to db tables.
  * - Learn how mobile applications are made using the compact framework.
  */
+
+/*
+ * In order to develop ASP.NET pages it is necessary to have IIS installed. 
+ * 
+ * In order to open the IIS configuration screen: Run - > inetmgr 
+ */
+
+/*
+ * When developing an ASP.NET web application, there are two main files.
+ * One of the files houses the HTML and the other file contains the logic.
+ * The HTML file is referred to as the "Web Forms page".
+ * The logic file is referred to as the "code-behind file".
+ * 
+ * The HTML/Web Forms page contains static HTML tags and any ASP.NET server
+ * controls that have been added to the application. 
+ * 
+ * The logic/code-behind file residents in a seperate file. All the event handler
+ * methods are stored in this code-behind file. This file contains the code that 
+ * enables the user to interact with the page. 
+ */
 namespace C_sharp_Programming
 {
     class Chapter14
